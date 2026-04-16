ea3d6b5ad0770dd121fc0a86074624c35bdb462c
OikawaD <uranus.oj@gmail.com>
2026-04-16 11:08:42 +0300

    chore: initialize spring boot project

---
+++ src/test/java/com/hellengi/biolab/BiolabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellengi.biolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
